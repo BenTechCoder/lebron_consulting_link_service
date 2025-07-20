3f945429b863fac260eb855116d2cf70fe0e47bc
BenTechCoder <benjaminnlebron@gmail.com>
2025-07-20 14:55:40 -0400

    start concepts for database and wp-api services

---
+++ src/server/services/database.ts
@@ -0,0 +1,12 @@
+
+/*
+Service to interact and manage SQlite DB Cache
+*/
+
+function syncWithWP(params:type) {
+    // Function that reloads local SQlite Cache with WP-API request Payload
+}
+
+function addLinkToDB(params:type) {
+ // Add individual Link to DB   
+}
---
+++ src/server/services/wp.ts
@@ -0,0 +1,3 @@
+/*
+Service for managing and interacting with the WP-API
+*/
