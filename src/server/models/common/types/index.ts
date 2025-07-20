eb2f627dae31b1e6f6ba11397098baa5c6ef2f08
BenTechCoder <benjaminnlebron@gmail.com>
2025-07-20 14:49:59 -0400

    add link interface concept

--- src/server/models/common/types/index.ts
+++ src/server/models/common/types/index.ts
@@ -1,5 +1,14 @@
-
 export interface IModel {
   id: number;
   created: Date;
+}
+
+export interface ILink {
+  id: number;
+  customSlug?: string;
+  title: string;
+  // TODO: Make/Grab/Vaidate Url as a type
+  redirectionUrl: string;
+  created: Date;
+  lastModified: Date;
 }
