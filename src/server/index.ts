fa8848423265144214e6aec9488cc7006761f718
BenTechCoder <benjaminnlebron@gmail.com>
2025-08-06 16:13:50 -0400

    add SQLite basics for cache layer

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,16 @@ import logger from 'jet-logger';
 
 import ENV from '@src/common/constants/ENV';
 import server from './server';
-
+import { startCache } from './services/database';
 
 /******************************************************************************
                                   Run
 ******************************************************************************/
 
-const SERVER_START_MSG = ('Express server started on http://localhost:' + 
-  ENV.Port.toString());
+const SERVER_START_MSG =
+  'Express server started on http://localhost:' + ENV.Port.toString();
 
-server.listen(ENV.Port, () => logger.info(SERVER_START_MSG));
+server.listen(ENV.Port, () => {
+  logger.info(SERVER_START_MSG);
+  startCache();
+});
--- src/server/services/database.ts
+++ src/server/services/database.ts
@@ -1,12 +1,34 @@
-
 /*
 Service to interact and manage SQlite DB Cache
 */
+import Database from 'better-sqlite3';
+import { promises as fs } from 'fs';
 
-function syncWithWP(params:type) {
-    // Function that reloads local SQlite Cache with WP-API request Payload
+const db = new Database('link_cache.db');
+db.pragma('journal_mode = WAL');
+
+export async function startCache() {
+  const isDbCreated = () => {
+    try {
+        fs.access('link_cache.db');
+        return true
+    } catch {
+        return false;
+    }
+  }
+  if (isDbCreated() === false) {
+    db.prepare(
+      `
+        CREATE TABLE links (id INTEGER PRIMARY KEY);
+        `
+    ).run();
+  }
 }
 
-function addLinkToDB(params:type) {
- // Add individual Link to DB   
+function syncWithWP(params: unknown) {
+  // Function that reloads local SQlite Cache with WP-API request Payload
+}
+
+function addLinkToDB(params: unknown) {
+  // Add individual Link to DB
 }
